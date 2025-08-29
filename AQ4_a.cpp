7c3233a4ea833e7fcedc1717c2b1340b3f602f94
Amit Bishnoi <amitbishnoe.2005@gmail.com>
2025-08-29 14:51:13 +0530

    Completed AQ1. Student ID: 1024240005

---
+++ AQ4_a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s1 = "hello ";
+    string s2 = "my name is ";
+    string s = s1+ s2 + "amit bishnoi" ;
+    cout << s; 
+    return 0;
+}
